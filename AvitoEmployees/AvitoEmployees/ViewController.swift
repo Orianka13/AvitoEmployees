b0e5c00084cd48afb7ba8c84648931725774863a
Orianka13 <odobrogor@gmail.com>
2022-10-24 18:14:11 +0300

    Создан проект

---
+++ AvitoEmployees/AvitoEmployees/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoEmployees
+//
+//  Created by Олеся Егорова on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
