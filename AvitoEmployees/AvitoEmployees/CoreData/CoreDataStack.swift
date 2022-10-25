3fc802c52018995849e738ff44bd7d91b3bc19ae
Orianka13 <odobrogor@gmail.com>
2022-10-25 16:34:54 +0300

    Добавлен модуль CoreData

---
+++ AvitoEmployees/AvitoEmployees/CoreData/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  AvitoEmployees
+//
+//  Created by Олеся Егорова on 25.10.2022.
+//
+
+import Foundation
--- AvitoEmployees/AvitoEmployees/ListModule/Model/EmployeeModel.swift
+++ AvitoEmployees/AvitoEmployees/ListModule/Model/EmployeeModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Employee {
+struct EmployeeModel {
     let name: String
     let phoneNumber: String
     let skills: [String]
